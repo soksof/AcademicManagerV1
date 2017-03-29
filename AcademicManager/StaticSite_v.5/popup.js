200dc0b0a5ca383796c71b75545f56dcb1884da9
instructor <instructor@mail.com>
2017-03-29 18:52:39 +0300

    Added files regarding: 	- Database initial creation script & ER models 	- Static web site

---
+++ AcademicManager/StaticSite_v.5/popup.js
@@ -0,0 +1,5 @@
+function popUp(URL) {
+
+eval("page" + " = window.open(URL, '" + "', 'toolbar=0,scrollbars=1,location=0,statusbar=,menubar=0,resizable=0,width=500,height=300,left = 440,top = 210');");
+
+}
