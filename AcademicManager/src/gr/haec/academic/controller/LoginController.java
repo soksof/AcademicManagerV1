162a463a97b4bf1f6bbdfa11b812f7e1cd0fd8c3
Elias <bastas.elias@windowslive.com>
2017-05-22 20:27:03 +0300

    fixed wrong credentials and fix the pop up message when the user do not exis

--- AcademicManager/src/gr/haec/academic/controller/LoginController.java
+++ AcademicManager/src/gr/haec/academic/controller/LoginController.java
@@ -27,34 +27,7 @@ public class LoginController extends HttpServlet {
 	}
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
- 
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		RequestDispatcher rd = null;
- 
-		PersonDao authenticator = new PersonDao();
-		Person p1 = authenticator.authenticate(username, password);
-		if (p1!=null) {//found a user
-			//Maybe create a Person instance
-			HttpSession session = request.getSession();
-			session.setAttribute("person", p1);
-			if(p1.getRole().equals(Role.secretary)){
-				rd = request.getRequestDispatcher("/home_secr.jsp");
-			}
-			else if(p1.getRole().equals(Role.teacher)){
-				rd = request.getRequestDispatcher("/home_teach.jsp");
-			}
-			else if(p1.getRole().equals(Role.student)){
-				rd = request.getRequestDispatcher("/home_stud.jsp");
-			}
-		} else {//Did not find the user
-			PrintWriter out=response.getWriter();
-			out.println("<script language='javascript' type='text/javascript'>");
-			out.println("alert(\"Invalid username or password!\")");
-			out.println("</script>");
-			rd = request.getRequestDispatcher("/index.html");
-		}
-		rd.forward(request, response);
+		this.doPost(request,response);
 	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
@@ -78,13 +51,16 @@ public class LoginController extends HttpServlet {
 			else if(p1.getRole().equals(Role.student)){
 				rd = request.getRequestDispatcher("/home_stud.jsp");
 			}
+			rd.forward(request, response);
 		} else {
+			rd = request.getRequestDispatcher("/index.html");
 			PrintWriter out=response.getWriter();
 			out.println("<script language='javascript' type='text/javascript'>");
 			out.println("alert(\"Invalid username or password!\")");
 			out.println("</script>");
-			rd = request.getRequestDispatcher("/index.html");
+		
+			rd.include(request, response);
+
 		}
-		rd.forward(request, response);
 	}
 }
